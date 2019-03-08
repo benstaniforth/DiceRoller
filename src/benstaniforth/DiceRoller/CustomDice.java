f17ef271a34aa687b1cf7fccc679b00cb160aa00
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-03-08 09:22:28 +0000

    Added interfaces

--- src/benstaniforth/DiceRoller/CustomDice.java
+++ src/benstaniforth/DiceRoller/CustomDice.java
@@ -1,10 +1,21 @@
 package benstaniforth.DiceRoller;
 
+import java.util.List;
+
 public class CustomDice implements Dice {
 
+    int numberOfSides;
+    List<Integer> customFaces;
+
+    public CustomDice(int numberOfSides, List<Integer> customFaces) {
+        this.numberOfSides = numberOfSides;
+        this.customFaces = customFaces;
+    }
 
     @Override
     public int roll() {
+
+
         return 0;
     }
 }
--- src/benstaniforth/DiceRoller/StandardDice.java
+++ src/benstaniforth/DiceRoller/StandardDice.java
@@ -1,10 +1,18 @@
 package benstaniforth.DiceRoller;
 
+import java.util.Random;
+
 public class StandardDice implements Dice {
 
+    int numberOfSides;
+
+    public StandardDice(int numberOfSides) {
+        this.numberOfSides = numberOfSides;
+    }
 
     @Override
     public int roll() {
-        return 0;
+        Random random = new Random();
+        return random.nextInt(numberOfSides) + 1;
     }
 }
